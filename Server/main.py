ec590e65f54b9b4a229450b78a77013780f3b61c
Ricardo Solórzano <131942537+Okami1530@users.noreply.github.com>
2025-03-18 15:50:30 -0600

    Ajustes de filtro, en base a pruebas

--- Server/main.py
+++ Server/main.py
@@ -1,11 +1,10 @@
-# Importar librerias
-import os
-import cv2
-import requests
 import numpy as np
-from flask import Flask, request, jsonify
+import cv2
+import os
+from flask import Flask, jsonify, request
+import requests
 
-# Configuración del bot de Telegram (Temporal)
+# Configuración del bot de Telegram
 TELEGRAM_BOT_TOKEN = 'TU_TOKEN_AQUI'
 TELEGRAM_CHAT_ID = 'TU_CHAT_ID_AQUI'
 MENSAJE_ALERTA = "¡Atención! Es hora de limpiar el arenero del gato."
@@ -20,36 +19,42 @@ def enviar_alerta():
 def detectar_heces(imagen_path):
     # Cargar imagen
     imagen = cv2.imread(imagen_path)
-
+    #grayscale = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
+    
+ 
     # Aplicar filtro de suavizado
-    suavizado = cv2.GaussianBlur(imagen, (15,15), 0)
+    suavizado = cv2.GaussianBlur(imagen, (41,41), 0)
 
     # Aplicar detección de bordes
-    bordes = cv2.Canny(suavizado, 20, 40, L2gradient=True)
+    bordes = cv2.Canny(suavizado, 20, 25)
 
     # Aplicar operaciones morfologicas
-    dilatado = cv2.dilate(bordes, None, iterations=3)
-    erosi = cv2.erode(dilatado, None, iterations=3)
 
-    #cv2.imshow('Bordes', erosi)
+    dilatado = cv2.dilate(bordes, None, iterations=3)
+    erosi = cv2.erode(dilatado, (5,5), iterations=3)
+    cv2.imshow('Bordes', erosi)
 
     countours, _ = cv2.findContours(erosi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
 
     # Dibujar los contornos detectados (opcional para depuración)
     cv2.drawContours(imagen, countours, -1, (0, 255, 0), 2)
 
+    # Mostrar la imagen (opcional para depuración)
+    cv2.imshow('Detección de Heces', imagen)
+    cv2.waitKey(3000)
+    cv2.destroyAllWindows()
 
     # Verificar si se detectaron 3 heces
     if len(countours) >= 3:
         print("🔔 ¡Tres heces detectadas! Enviando alerta...")
+        print(f"✅ Número de heces detectadas: {len(countours)}")
         enviar_alerta()
     else:
         print(f"✅ Número de heces detectadas: {len(countours)}")
 
 # ----------------- PROGRAMA PRINCIPAL -----------------
 app = Flask(__name__)
-@app.route('/upload', methods=['POST'])
+@app.route("/upload", methods=["POST"])    
 def upload():
     if 'imagen' not in request.files:
         return jsonify({"error": "No se encontró la imagen en la solicitud."}), 400
@@ -64,5 +69,9 @@ def upload():
     return jsonify({"mensaje": "Imagen recibida y procesada correctamente."})
 
 if __name__ == "__main__":
+    # Imagen capturada por el ESP32-CAM (ejemplo)
+    ruta_imagen = "images/imagen0.jpg"
     os.makedirs("images", exist_ok=True)
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
+    # Detectar heces y enviar alerta si se cumplen las condiciones
+    detectar_heces(ruta_imagen)
